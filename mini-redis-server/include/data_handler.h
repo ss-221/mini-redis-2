b05432d3d6c2af7612d426404d5941a10d25d147
ss-221 <shubham.suryawanshi.22.1@gmail.com>
2023-07-09 11:36:01 +0530

    Class creation and handling GET

--- mini-redis-server/include/data_handler.h
+++ mini-redis-server/include/data_handler.h
@@ -5,15 +5,40 @@
 #include <sstream>
 #include <vector>
 #include <iostream>
+#include <chrono>
+#include <unordered_map>
+#include <mutex>
 
 namespace data_handler
 {
-
+    using namespace std::chrono;
+    using std::string;
+    using std::vector;
     std::string ProcessCommand(char*);
+    
+    class MetaData
+    {
+        public:
+        MetaData() = default;
+        string getValue() const;
+
+        private:
+            string m_Value;
+            milliseconds m_ExpiryTime;
+    };
 
     class DB
     {
-
+        public:
+            string HandleSET(vector<string>&);
+            string HandleGET(vector<string>&);
+            string HandleDEL(vector<string>&);
+            string HandleTTL(vector<string>&);
+            string HandleEXPIRE(vector<string>&);
+            string HandleKEYS(vector<string>&);
+        private:
+            std::mutex m_lock;
+            std::unordered_map<string, MetaData> dbKeys;
     };
 
 
--- mini-redis-server/src/data_handler.cpp
+++ mini-redis-server/src/data_handler.cpp
@@ -4,7 +4,10 @@
 
 namespace data_handler
 {
+
+    DB db;
     using std::cout;
+
     std::string ProcessCommand(char* command)
     {
         std::stringstream ss(command);
@@ -18,27 +21,32 @@ namespace data_handler
 
         if(tokens.front() == "SET")
         {
-            temp = "SET";
+            temp = db.HandleSET(tokens);
         }
 
         else if(tokens.front() == "GET")
         {
-            temp = "GET";
+            temp = db.HandleGET(tokens);
         }
 
         else if(tokens.front() == "DEL")
         {
-            temp = "DEL";
+            temp = db.HandleDEL(tokens);
         }
 
         else if(tokens.front() == "TTL")
         {
-            temp = "TTL";
+            temp = db.HandleTTL(tokens);
         }
 
         else if(tokens.front() == "EXPIRE")
         {
-            temp = "EXPIRE";
+            temp = db.HandleEXPIRE(tokens);
+        }
+
+        else if(tokens.front() == "KEYS")
+        {
+            temp = db.HandleKEYS(tokens);
         }
 
         else
@@ -48,7 +56,47 @@ namespace data_handler
             temp += "'" + tokens.front() + "'";
         }
 
-
         return temp;
+    }
+
+    string MetaData::getValue() const
+    {
+        return m_Value;
+    }
+
+    string DB::HandleSET(vector<string>& tokens)
+    {
+        return "SET";
+    }
+
+    string DB::HandleGET(vector<string>& tokens)
+    {
+        std::scoped_lock lock(m_lock);
+        if(dbKeys.find(tokens.front()) == dbKeys.end())
+        {
+            return "(nil)";
+        }
+
+        return dbKeys[tokens.front()].getValue();
+    }
+
+    string DB::HandleDEL(vector<string>& tokens)
+    {
+        return "DEL";
+    }
+
+    string DB::HandleTTL(vector<string>& tokens)
+    {
+        return "TTL";
+    }
+
+    string DB::HandleEXPIRE(vector<string>& tokens)
+    {
+        return "EXPIRE";
+    }
+
+    string DB::HandleKEYS(vector<string>& tokens)
+    {
+        return "KEYS";
     }
 }
